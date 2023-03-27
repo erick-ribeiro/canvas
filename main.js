554175dfadda7a7b83f7cce1975bb65df8be6613
Erick Ribeiro <gustavoribeirerick@gmail.com>
2023-03-27 08:27:16 -0300

    inclusao do tiro ao alvo

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var display = document.querySelector("canvas");
 var pen = display.getContext('2d');
+let colorChose = document.querySelector('input');
 let colorPen = ['green', 'red', 'blue'];
 let iColor = 0;
+let radius = 10
 
 // drawRect(0, 0, 'green');
 // drawRect(50, 0, 'green');
@@ -9,27 +11,56 @@ display.onclick = draw;
 display.oncontextmenu = changeColor;
 
 
+drawTarget(300,200, radius + 80, 'red');
+drawTarget(300,200, radius + 60, 'white');
+drawTarget(300,200, radius + 40, 'red');
+drawTarget(300,200, radius + 20, 'white');
+drawTarget(300,200, radius, 'red');
+
+
 var x = 0;
 while(x < 600) {
-    drawRect(x, 0, 'blue');
+    // drawRect(x, 0, 'blue', 50, 50);
     x += 50;
 }
 
-function drawRect(x, y, color){
+function drawRect(x, y, color, width, height){
     pen.fillStyle = color;
-    pen.fillRect( x, y, 50, 50);
+    pen.fillRect( x, y, width, height);
     pen.fillStroke = 'black'
-    pen.strokeRect(x, y, 50, 50);
+    pen.strokeRect(x, y, width, height);
+}
+
+function drawTarget(x, y, r, color){
+    pen.fillStyle = color;
+    pen.beginPath();
+    pen.arc(x, y, r, 0, 2 * Math.PI);
+    pen.fill();
 }
 
 function draw(event){
     var pageX = event.pageX - display.offsetLeft;
     var pageY = event.pageY - display.offsetTop;
+    
 
-    pen.fillStyle = colorPen[iColor];
+    // pen.fillStyle = colorPen[iColor];
+    pen.fillStyle = colorChose.value;
     pen.beginPath();
-    pen.arc(pageX, pageY, 10, 0, 2 * 3.14);
+    if (event.shiftKey) {
+       radius *= 5;
+    };
+    if (event.ctrlKey) {
+        radius /= 5;
+    }
+    pen.arc(pageX, pageY, radius, 0, 2 * 3.14);
     pen.fill();
+
+    if ((pageX > 300 - radius) &&
+        (pageX < 300 + radius) &&
+        (pageY > 200 - radius) &&
+        (pageY < 200 + radius)) {
+        alert('Acertou no centro do alvo');
+    }
 
     // alert(pageX + ', ' + pageY);
     console.log(event);
@@ -43,4 +74,8 @@ function changeColor(){
     console.log(iColor)
     return false;
     
+}
+
+function upPen(){
+
 }
